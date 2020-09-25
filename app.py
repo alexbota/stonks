882e7bedd74d7ac9a44c6dcf9016a892b28dd4ca
Alex Bota <botavalex@gmail.com>
2020-09-25 17:50:52 +0200

    added app config

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ app = Flask(__name__)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+app.config["SESSION_FILE_DIR"] = mkdtemp()
 # Ensure responses aren't cached
 
 
